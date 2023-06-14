a8b1ae4fe7b374217eb6b43e46e6649062b7ba6b
vladimirlukyanov <vladimir@lukyanov.net>
2023-06-14 03:48:10 +0300

    v0.1.3 – Content update

--- src/schemas/categories.ts
+++ src/schemas/categories.ts
@@ -1,6 +1,7 @@
 // 1. Import utilities from `astro:content`
 import { z } from 'astro:content';
 export const categoriesSchema = z.object({
+    id: z.number().int(),
     title: z.string(),
     featured_image: z.string()
 })
