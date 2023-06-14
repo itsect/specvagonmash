1dc6d9724dd77f92ffeaa1f9e9652698f0954bbf
vladimirlukyanov <vladimir@lukyanov.net>
2023-06-14 08:07:19 +0300

    v0.2.6 – project update

--- src/schemas/categories.ts
+++ src/schemas/categories.ts
@@ -3,5 +3,7 @@ import { z } from 'astro:content';
 export const categoriesSchema = ({ image }) => z.object({
     id: z.number().int(),
     title: z.string(),
-    image: image()
+    image: image().refine((img) => img.width >= 1080, {
+        message: "Cover image must be at least 1080 pixels wide!",
+    })
 })
