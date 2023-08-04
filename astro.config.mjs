3d9122bf87c908d87675529af15c9bb8617ef518
vladimirlukyanov <vladimir@lukyanov.net>
2023-08-04 03:49:04 +0300

    v0.8.1 - path update

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,8 @@ import sitemap from '@astrojs/sitemap';
 
 
 export default defineConfig({
-    site: 'http://localhost:3000',
-    // base: '/specvagonmash',
+    site: 'https://itsect.github.io/specvagonmash',
+    base: '/specvagonmash',
     integrations: [react(), mdx(), sitemap(), tailwind({
         configFile: fileURLToPath(new URL('./tailwind.config.cjs', import.meta.url)),
         // applyBaseStyles: true,
